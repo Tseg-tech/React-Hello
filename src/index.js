86193c3d224e7505ed5593f5405186a52a25310f
Tseg-tech <tseganehamare@gmail.com>
2025-04-07 16:49:27 +0300

    Working With JSX

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
  import React from 'react';
  import ReactDOM from 'react-dom/client';
+  
+ //jsx 
+function HelloWorld(){
+    return(
+        <div>
+           <Hello/> <World/> !
+        </div>
+    );
+}
+function Hello(){
+    return(
+        <div>
+            Hello
+        </div>
+  );
+}
 
+function World(){
+    return(
+       <div>
+        World
+      </div>
+    );  
+}
+  //compiled jsx i.e, js which is turned by babel compiler
+/*
  function HelloWorld(){
     return React.createElement(
         'span',
@@ -10,10 +35,28 @@
          
     );
  }
+/*
+ function songName(props){
+    return (
+        <span className='song-name'>
+           {props.song.name}
+        </span>
+    );
+ }
 
+ function songName(props){
+    return React.createElement(
+        'span',//string|Element
+        {className :'song-name'},//props object
+        props.song.name //children
+    )
+ }
+*/
  ReactDOM.createRoot(document.getElementById('root')).render(
     
-        <HelloWorld/>
+        <HelloWorld/>,
+        
+
     
  //document.querySelector('#root')
 );
